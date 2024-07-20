9f3337e2e1075da075ada672d7ef2968cbc209dd
Michael Fitzgerald <michael.fitzgerald.1406@gmail.com>
2024-07-20 22:57:31 +0900

    removed uncessary console.logs and comments

--- content.js
+++ content.js
@@ -1,7 +1,3 @@
-// content.js
-console.log("Button Move: Script started");
-
-// Debounce function to limit how often moveDiv is called
 function debounce(func, wait) {
     let timeout;
     return function executedFunction(...args) {
@@ -13,11 +9,7 @@ function debounce(func, wait) {
         timeout = setTimeout(later, wait);
     };
 }
-
 function moveDiv() {
-    console.log("Button Move: moveDiv function called");
-
-    // Function to get element by XPath
     function getElementByXpath(path) {
         return document.evaluate(
             path,
@@ -27,22 +19,15 @@ function moveDiv() {
             null
         ).singleNodeValue;
     }
-
-    // XPaths of the divs
     const divXPath = "/html/body/main/div/div[3]/div/div[4]";
     const afterXPath = "/html/body/main/div/div[3]/div/div[1]";
     const parentXPath = "/html/body/main/div/div[3]/div";
-
-    // Select the divs using XPath
     const divToMove = getElementByXpath(divXPath);
     const targetDiv = getElementByXpath(afterXPath);
     const parentDiv = getElementByXpath(parentXPath);
-
-    // Move the div if both elements are found
     if (divToMove && targetDiv && parentDiv) {
         if (targetDiv.nextElementSibling !== divToMove) {
             parentDiv.insertBefore(divToMove, targetDiv.nextElementSibling);
-            console.log("Button Move: Div moved successfully");
         } else {
             console.log("Button Move: Div already in correct position");
         }
@@ -50,14 +35,8 @@ function moveDiv() {
         console.log("Button Move: One or more divs not found");
     }
 }
-
-// Debounced version of moveDiv
 const debouncedMoveDiv = debounce(moveDiv, 250);
-
-// Run moveDiv once when the script loads
 moveDiv();
-
-// Set up a MutationObserver to watch for changes in the specific parent div
 function setupObserver() {
     const parentXPath = "/html/body/main/div/div[3]/div";
     const parentDiv = document.evaluate(
@@ -67,16 +46,11 @@ function setupObserver() {
         XPathResult.FIRST_ORDERED_NODE_TYPE,
         null
     ).singleNodeValue;
-
     if (parentDiv) {
         const observer = new MutationObserver(debouncedMoveDiv);
         observer.observe(parentDiv, { childList: true, subtree: true });
-        console.log("Button Move: Observer set up successfully");
     } else {
-        console.log("Button Move: Parent div not found, retrying in 1 second");
         setTimeout(setupObserver, 1000);
     }
 }
-
-// Start trying to set up the observer
 setupObserver();
